0bf2b1cac60ac25867d4b8feae08fa8401ea94da
arpita595 <arpitapatil595@gmail.com>
2026-09-18 13:47:57 +0530

    security: add Checkov policy config and OIDC IAM trust relationship

---
+++ envs/dev/oidc.tf
@@ -0,0 +1,31 @@
+resource "aws_iam_openid_connect_provider" "github" {
+  url             = "https://token.actions.githubusercontent.com"
+  client_id_list  = ["sts.amazonaws.com"]
+  thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
+}
+
+data "aws_iam_policy_document" "github_trust" {
+  statement {
+    effect = "Allow"
+    principals {
+      type        = "Federated"
+      identifiers = [aws_iam_openid_connect_provider.github.arn]
+    }
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    condition {
+      test     = "StringEquals"
+      variable = "token.actions.githubusercontent.com:aud"
+      values   = ["sts.amazonaws.com"]
+    }
+    condition {
+      test     = "StringLike"
+      variable = "token.actions.githubusercontent.com:sub"
+      values   = ["repo:YOUR_GH_USERNAME/secure-landing-zone:*"]
+    }
+  }
+}
+
+resource "aws_iam_role" "github_actions" {
+  name               = "github-actions-landingzone"
+  assume_role_policy = data.aws_iam_policy_document.github_trust.json
+}
