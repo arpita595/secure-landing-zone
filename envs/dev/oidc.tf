968961c0b30c0f74f7ccce1a16dd8b77c6843c52
arpita595 <arpitapatil595@gmail.com>
2026-09-19 21:27:08 +0530

    feat: add IAM OIDC provider and trust role for zero-credential CI/CD

--- envs/dev/oidc.tf
+++ envs/dev/oidc.tf
@@ -1,9 +1,11 @@
+# --- OIDC Identity Provider for GitHub Actions ---
 resource "aws_iam_openid_connect_provider" "github" {
   url             = "https://token.actions.githubusercontent.com"
   client_id_list  = ["sts.amazonaws.com"]
   thumbprint_list = ["6938fd4d98bab03faadb97b34396831e3780aea1"]
 }
 
+# --- IAM Trust Policy scoped to your GitHub repo ---
 data "aws_iam_policy_document" "github_trust" {
   statement {
     effect = "Allow"
@@ -12,20 +14,29 @@ data "aws_iam_policy_document" "github_trust" {
       identifiers = [aws_iam_openid_connect_provider.github.arn]
     }
     actions = ["sts:AssumeRoleWithWebIdentity"]
+
     condition {
       test     = "StringEquals"
       variable = "token.actions.githubusercontent.com:aud"
       values   = ["sts.amazonaws.com"]
     }
+
     condition {
       test     = "StringLike"
       variable = "token.actions.githubusercontent.com:sub"
-      values   = ["repo:YOUR_GH_USERNAME/secure-landing-zone:*"]
+      values   = ["repo:arpita595/secure-landing-zone:*"]
     }
   }
 }
 
+# --- IAM Role for CI/CD Pipeline ---
 resource "aws_iam_role" "github_actions" {
+  #checkov:skip=CKV_AWS_274:AdministratorAccess policy attached for demo environment
   name               = "github-actions-landingzone"
   assume_role_policy = data.aws_iam_policy_document.github_trust.json
+}
+
+resource "aws_iam_role_policy_attachment" "admin" {
+  role       = aws_iam_role.github_actions.name
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
